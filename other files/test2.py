035cebc222e66d817e9c7e29799e633e7a015bd0
CapnBloodBeard <capnbloodbeard.yt@gmail.com>
2022-01-25 22:55:50 +0530

    update

--- other files/test2.py
+++ other files/test2.py
@@ -3,3 +3,4 @@ import random
 print(str(int(math.pow(random.randint(2,4),2))),end='')
 print(str(int(math.pow(random.randint(2,4),2))),end='')
 print(str(int(math.pow(random.randint(2,4),2))))
+#helloww
