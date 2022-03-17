16e2505319a22417ca77cd2ac30a859686cdc190
CapnBloodBeard <capnbloodbeard.yt@gmail.com>
2022-03-17 16:33:16 +0530

    Fule Updates

---
+++ Python Files/Practice Files/Modules Practice/file_2.py
@@ -0,0 +1,5 @@
+def test1():
+    print(123)
+
+def test2():
+    print(234)
---
+++ Python Files/Practice Files/file 11.py
@@ -0,0 +1,16 @@
+'''dict1={'lamp':'bulb', 'pen':'refil'}
+dict2={1:123 , 2:'apple' , 'test':1234}
+
+
+print(dict1)
+
+x=dict2[2]
+dict1['switch']=dict2['test']
+
+print(dict1)
+
+
+'''
+dict2={1:123 , 2:'apple' , 'test':1234}
+for i in dict2.values():
+    print(i)
