5dfd5bd2d35f62e19d6ad82651d23c447cf1b460
CapnBloodBeard <77095391+CapnBloodBeard@users.noreply.github.com>
2022-10-12 19:44:09 +0530

    Codewars Update

---
+++ Python Files/Practice Files/CodeWars/Code1.py
@@ -0,0 +1,18 @@
+
+def Ques1():
+    def alphabet_position(text):
+        nums=''
+        alp = "abcdefghijklmnopqrstuvwxyz"
+        for i in text.lower():
+            if i in alp:
+                y=str(alp.find(i)+1)
+                nums+=str(y)+' '
+        ans=nums
+        return ans
+
+    x=alphabet_position("The sunset sets at twelve o' clock.")
+    print(x)
+
+def Ques2():
+    def duplicate_count(text):
+    # Your code goes here
