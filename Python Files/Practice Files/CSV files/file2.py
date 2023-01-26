8384b4c35c8bceb3d47db60b3587fad11627ae2e
CapnBloodBeard <77095391+CapnBloodBeard@users.noreply.github.com>
2023-01-26 21:20:31 +0530

    csv file update

---
+++ Python Files/Practice Files/CSV files/file2.py
@@ -0,0 +1,14 @@
+import csv
+fields=['name','class','year','percent']
+choice=int(input("enter the number of entries:"))
+f=open("Python Files\Practice Files\CSV files\stu1.csv",'w')
+csvv=csv.writer(f)
+csvv.writerow(fields)
+for i in range (choice):
+    name=input("enter name: ")
+    classt=input("enter class: ")
+    year=input("enter year: ")
+    percent=input("enter percentage: ")
+
+    csvv.writerow([name,classt,year,percent])
+    print("file created")
