d822626042984ac695cbdea1cc172666f73b9564
CapnBloodBeard <77095391+CapnBloodBeard@users.noreply.github.com>
2025-04-23 21:09:08 +0530

    FIle Updates + RPG Game Update

--- Text Game/2.0/perks_02.py
+++ Text Game/2.0/perks_02.py
@@ -4,31 +4,6 @@ from Main import user_name
 
 
 
-def charater_select_check():
-    
-    global perk_range_veryhigh,perk_range_high,perk_range_med,perk_range_low,perk_range_verylow
-    global select_character , select_character_str
-
-    print('\nPick one of the Following Classes. . .Each has its own Benefits, choose wisely!')
-    time.sleep(2.4)
-    print("\nClasses: \n 1. Brute : 120-HP High Damage, Med Defence, Low Speed, Low Crit \n 2. Archer: 90-HP Med Damage, Low Defence, High Speed, High Crit \n 3. Warrior: 100-HP Med Damage, High Defence, Med Speed, Med Crit \n 4. Assassin: 90-HP Low Damage, Low Defence, High Speed, High Crit\n")
-    
-    #perks limits
-    perk_range_veryhigh=random.randint(100, 190)
-    perk_range_high=random.randint(90, 150)
-    perk_range_med=random.randint(50, 120)
-    perk_range_low=random.randint(10, 100)
-    perk_range_verylow=random.randint(5, 50)
-
-
-
-    select_character_str=str(input('Press the Number 1-4 to select your character. . .'))
-    #--------------------------------
-    return character_select()
-
-
-
-#----------------------------------------------------------------------------------------------------------------------
 class perks:
     def __init__(self , health ,strength , defence , speed , crit) -> None:
         self.perk_health = health
